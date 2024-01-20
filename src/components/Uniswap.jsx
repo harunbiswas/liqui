ebd31a896dc71993084d3e919c3d4fdf9bb01e13
Harun BIswas Rubel <harunbiswasrubel@gmail.com>
2024-01-21 01:36:29 +0600

    update

--- src/components/Uniswap.jsx
+++ src/components/Uniswap.jsx
@@ -1,14 +1,19 @@
+// Import necessary modules and components
 import { SwapWidget } from '@uniswap/widgets'
 import '@uniswap/widgets/fonts.css'
 import React, { useCallback, useState } from 'react'
 import Title from './basic/Title'
 
 // Define SupportedLocale type
-// type SupportedLocale = 'en-US'
+// type SupportedLocale = 'en-US';
 
 const TOKEN_LIST = 'https://gateway.ipfs.io/ipns/tokens.uniswap.org'
 const JSON_RPC_URL = 'https://cloudflare-eth.com'
 const UNI = '0xfcC16F9CAb16ec0D0586E5c0DC96AfdE9f78148b'
+
+const babelPlugin = '@babel/plugin-proposal-private-property-in-object'
+
+// Add autoprefixer and eslint configurations if needed
 
 export default function Uniswap() {
   const [locale, setLocale] = useState('en-US')
@@ -41,4 +46,8 @@ export default function Uniswap() {
   )
 }
 
-/* <iframe width="100%" height={700} src="https://app.uniswap.org/swap?outputCurrency=0xfcC16F9CAb16ec0D0586E5c0DC96AfdE9f78148b" frameborder="0"></iframe> */
+// Ensure that your Babel configuration includes the plugin
+// Example .babelrc file:
+// {
+//   "plugins": ["@babel/plugin-proposal-private-property-in-object"]
+// }
